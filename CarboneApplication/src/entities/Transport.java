e9801e39d994a3e1dd615ea56b3ec0dcbd8d37a9
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-09-13 17:06:44 +0100

    😁 added the jar file

--- CarboneApplication/src/entities/Transport.java
+++ CarboneApplication/src/entities/Transport.java
@@ -24,6 +24,6 @@ public class Transport extends Consumption {
     @Override
     public double calculateImpact() {
         double impactPerDistance = (vehicleType.equals("CAR") ? 0.5 : 0.1);
-        return impactPerDistance * distanceTraveled;
+        return impactPerDistance * distanceTraveled * getAmount();
     }
 }
